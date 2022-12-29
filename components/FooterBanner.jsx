391ef8d0116378088d7bd9a3ee6fd02b67408583
AndyHu8 <huandy01@gmail.com>
2022-12-29 17:59:30 +0100

    Startseite Responsive

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -5,24 +5,20 @@ const FooterBanner = ({ footerBanner: { largeText1, largeText2, smallText, midTe
   return (
     <div className='footer-banner-container'>
       <div className='banner-desc'>
-      <div className='left'>
-        {/* <p>{discount}</p> */}
-        <h3>{largeText1}</h3>
-        <h3>{largeText2}</h3>
-        {/* <p>{saleTime}</p> */}
-      </div>
+        <div className='left'>
+          {/* <p>{discount}</p> */}
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          {/* <p>{saleTime}</p> */}
+        </div>
 
-      <div className='right'>
-        <p>{smallText}</p>
-        <h3>{midText}</h3>
-        <p>{desc}</p>
-        {/* <Link href={`/product/${product}`}>
-          <button type='button'>{buttonText}</button>
-        </Link> */}
-      </div>
+        <img className='footer-banner-image' src={urlFor(image)} alt="Footer Banner" />
 
-      <img className='footer-banner-image' src={urlFor(image)} alt="Footer Banner" />
-
+        <div className='right'>
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+        </div>
       </div>
     </div>
   )
