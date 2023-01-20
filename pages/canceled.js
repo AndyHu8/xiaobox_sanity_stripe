8d909a6e859cbc00b6ac4e0b2a39cd7a8db96f01
AndyHu8 <huandy01@gmail.com>
2023-01-20 21:36:22 +0100

    Cancel Page + kleine Fixes

---
+++ pages/canceled.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react'
+import Link from 'next/link'
+import { BsBagCheckFill } from 'react-icons/bs'
+import { useStateContext } from '../context/StateContext'
+
+const Canceled = () => {
+    const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+
+    useEffect(() => {
+        localStorage.clear();
+        setCartItems([]);
+        setTotalPrice(0);
+        setTotalQuantities(0);
+    }, []);
+
+    return (
+        <div className='success-wrapper'>
+            <div className='success'>
+                <h2>Etwas ist schief gelaufen :(</h2>
+                <p className='description'>Falls du Fragen hast, dann schreibe eine Mail an <a className='email' href='mailto:huandy01@gmail.com'>huandy01@gmail.com</a></p>
+                <Link href='/'>
+                    <button className='btn' type='button' width='300px'>Zurück zum Shop</button>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Canceled
