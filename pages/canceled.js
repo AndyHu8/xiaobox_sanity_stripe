6f4a44a14739c7f4ae218d1494fbc4bb7b44bc4e
AndyHu8 <huandy01@gmail.com>
2023-01-21 20:16:16 +0100

    import entfernt

--- pages/canceled.js
+++ pages/canceled.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react'
 import Link from 'next/link'
-import { BsBagCheckFill } from 'react-icons/bs'
 import { useStateContext } from '../context/StateContext'
 
 const Canceled = () => {
